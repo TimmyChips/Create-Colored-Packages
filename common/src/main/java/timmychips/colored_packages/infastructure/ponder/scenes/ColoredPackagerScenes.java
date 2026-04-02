8af217a9b8cec16ae7a37cafc926cce9ff996b37
TimmyChips <jhinners@outlook.com>
2026-04-02 15:23:19 -0500

    Ponder scene should be good now, lang still needs to be done

--- common/src/main/java/timmychips/colored_packages/infastructure/ponder/scenes/ColoredPackagerScenes.java
+++ common/src/main/java/timmychips/colored_packages/infastructure/ponder/scenes/ColoredPackagerScenes.java
@@ -33,52 +33,53 @@ public class ColoredPackagerScenes {
         scene.showBasePlate();
 
         Selection chest1 = util.select()
-                .position(2, 2, 2);
+                .position(2, 2, 3);
         BlockPos funnel1 = util.grid()
-                .at(1, 2, 1);
+                .at(1, 2, 2);
         Selection funnel1S = util.select()
                 .position(funnel1);
         BlockPos packager1 = util.grid()
-                .at(2, 2, 1);
+                .at(2, 2, 2);
         Selection packager1S = util.select()
                 .position(packager1);
         Selection largeCog = util.select()
-                .position(4, 0, 2);
+                .position(4, 0, 3);
         Selection cogNBelt = util.select()
-                .fromTo(3, 1, 1, 0, 1, 1)
+                .fromTo(3, 1, 2, 0, 1, 2)
                 .add(util.select()
-                        .position(3, 1, 2));
+                        .fromTo(4, 1, 3, 3, 1, 3));
         BlockPos lever = util.grid()
-                .at(2, 3, 1);
+                .at(2, 3, 2);
         Selection scaff1 = util.select()
-                .position(2, 1, 2);
+                .position(2, 1, 3);
         scene.idle(5);
 
+        // Slide transition sections in
         scene.world()
                 .showSection(chest1, Direction.UP);
         scene.world()
                 .showSection(funnel1S, Direction.UP);
-        ElementLink<WorldSectionElement> leverL = scene.world()
-                .showIndependentSection(util.select()
-                        .position(lever), Direction.DOWN);
+        scene.world()
+                .showSection(util.select()
+                .position(lever), Direction.DOWN);
         scene.world()
                 .showSection(largeCog, Direction.UP);
         scene.world()
                 .showSection(cogNBelt, Direction.SOUTH);
         scene.world()
                 .showSection(scaff1, Direction.UP);
-        ElementLink<WorldSectionElement> packager1L = scene.world()
+        scene.world()
                 .showIndependentSection(packager1S, Direction.SOUTH);
-//        scene.world().setBlocks(packager1S, AllBlocks.PACKAGER.getDefaultState(), false);
         scene.idle(20);
 
+        // Default packager overlay
         scene.overlay()
                 .showText(80)
                 .text("By default, packagers will create standard packages")
                 .attachKeyFrame()
                 .placeNearTarget()
                 .pointAt(util.vector()
-                        .of(2, 3, 1.5));
+                        .of(2, 3, 2.5));
         scene.idle(60);
 
         // Activate lever + package; create default package
@@ -86,8 +87,7 @@ public class ColoredPackagerScenes {
                 .toggleRedstonePower(util.select()
                         .fromTo(lever, packager1));
         scene.effects()
-                .indicateRedstone(lever.west(2)
-                        .below());
+                .indicateRedstone(lever);
         ItemStack box = PackageStyles.getDefaultBox()
                 .copy();
         PackageItem.addAddress(box, "Warehouse");
@@ -96,44 +96,37 @@ public class ColoredPackagerScenes {
 
         // Default package on belt
         scene.world()
-                .setKineticSpeed(util.select() // NBT has belt at 0 RPM so we use setKineticSpeed(); also needs to be above createItemOnBelt() so item appears
-                        .everywhere(), 32f);
-        scene.world()
                 .createItemOnBelt(util.grid()
-                        .at(1, 1, 1), Direction.EAST, box);
+                        .at(1, 1, 2), Direction.EAST, box);
         PonderHilo.packagerClear(scene, packager1);
         scene.world()
                 .toggleRedstonePower(util.select()
-                        .fromTo(2, 2, 1, 2, 3, 1));
+                        .fromTo(2, 2, 2, 2, 3, 2));
         scene.idle(30);
 
         // Package hops off belt; item still technically present lmao
         PonderHilo.packageHopsOffBelt(scene, util.grid()
-                .at(0, 1, 1), Direction.WEST, box);
+                .at(0, 1, 2), Direction.WEST, box);
         scene.world().modifyEntities(ItemEntity.class, Entity::discard); // Kill package item entity
         scene.idle(20);
 
         // Dye overlay
-        scene.world()
-                .setKineticSpeed(util.select() // Stop belt
-                        .everywhere(), 0f);
         scene.overlay()
                 .showOutlineWithText(util.select()
-                        .position(2, 2, 1), 40)
+                        .position(2, 2, 2), 40)
                 .text("Right clicking on a Packager or Re-Packager will dye it")
                 .colored(PonderPalette.GREEN)
                 .placeNearTarget()
                 .attachKeyFrame()
                 .pointAt(util.vector()
-                        .blockSurface(util.grid()
-                                .at(2, 2, 1), Direction.WEST));
+                        .of(2, 3, 2.5));
         scene.idle(60);
 
         // Dyed packager
         ItemStack applied = new ItemStack(Items.RED_DYE);
         scene.overlay()
                 .showControls(util.vector()
-                        .of(2.5, 3, 1.5), Pointing.DOWN, 40)
+                        .of(2.5, 3, 2.5), Pointing.DOWN, 40)
                 .withItem(applied);
         scene.idle(10);
 
@@ -148,8 +141,7 @@ public class ColoredPackagerScenes {
                 .placeNearTarget()
                 .attachKeyFrame()
                 .pointAt(util.vector()
-                        .blockSurface(util.grid()
-                                .at(2, 2, 1), Direction.WEST));
+                        .of(2, 3, 2.5));
         scene.idle(40);
 
         // Activate lever + package; colored package
@@ -157,8 +149,7 @@ public class ColoredPackagerScenes {
                 .toggleRedstonePower(util.select()
                         .fromTo(lever, packager1));
         scene.effects()
-                .indicateRedstone(lever.west(2)
-                        .below());
+                .indicateRedstone(lever);
         box = ColoredPackageStyles.getDefaultColoredBox()
                 .copy();
         PackageItem.addAddress(box, "Warehouse");
@@ -167,19 +158,16 @@ public class ColoredPackagerScenes {
 
         // Colored package on belt
         scene.world()
-                .setKineticSpeed(util.select() // Stop belt
-                        .everywhere(), 32f);
-        scene.world()
                 .createItemOnBelt(util.grid()
-                        .at(1, 1, 1), Direction.EAST, box);
+                        .at(1, 1, 2), Direction.EAST, box);
         PonderHilo.packagerClear(scene, packager1);
         scene.world()
                 .toggleRedstonePower(util.select()
-                        .fromTo(2, 2, 1, 2, 3, 1));
+                        .fromTo(2, 2, 2, 2, 3, 2));
         scene.idle(30);
         // Package hops off belt; item still technically present lmao
         PonderHilo.packageHopsOffBelt(scene, util.grid()
-                .at(0, 1, 1), Direction.WEST, box);
+                .at(0, 1, 2), Direction.WEST, box);
         scene.world().modifyEntities(ItemEntity.class, Entity::discard); // Kill package item entity
         scene.idle(20);
 
@@ -190,18 +178,19 @@ public class ColoredPackagerScenes {
                 .placeNearTarget()
                 .attachKeyFrame()
                 .pointAt(util.vector()
-                        .blockSurface(util.grid()
-                                .at(2, 2, 1), Direction.WEST));
+                        .of(2, 3, 2.5));
         scene.idle(40);
 
         applied = new ItemStack(Items.WATER_BUCKET);
         scene.overlay()
                 .showControls(util.vector()
-                        .of(2.5, 3, 1.5), Pointing.DOWN, 40)
+                        .of(2.5, 3, 2.5), Pointing.DOWN, 40)
                 .withItem(applied);
         scene.idle(10);
 
         scene.world().setBlocks(packager1S, AllBlocks.PACKAGER.getDefaultState(), false);
         scene.idle(40);
+
+        /// Scene end
     }
 }
