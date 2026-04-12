d90c179829d4c34dd96a26cc91a50d9bb1f59e90
TimmyChips <jhinners@outlook.com>
2026-04-12 13:11:16 -0500

    Frogport Renderer Mixin now renders the correct package

--- forge/src/main/java/timmychips/colored_packages/forge/mixin/FrogportRendererMixinForge.java
+++ forge/src/main/java/timmychips/colored_packages/forge/mixin/FrogportRendererMixinForge.java
@@ -11,6 +11,7 @@ import net.createmod.catnip.render.SuperByteBuffer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -29,7 +30,7 @@ import timmychips.colored_packages.content.logistics.box.util.ColoredPackagePart
 @Mixin(FrogportRenderer.class)
 public class FrogportRendererMixinForge {
 
-    @Inject(method = "renderPackage", at = @At("HEAD"))
+    @Inject(method = "renderPackage", at = @At("HEAD"), cancellable = true)
     public void coloredPackages$renderPackager(FrogportBlockEntity blockEntity, PoseStack ms, MultiBufferSource buffer, int light, int overlay, Vec3 diff, float scale, float itemDistance, CallbackInfo ci) {
         if (blockEntity.animatedPackage != null) {
             if (!((double)scale < 0.45)) {
@@ -40,27 +41,19 @@ public class FrogportRendererMixinForge {
                 /// Set model to either colored package based on the animatedPackage PackageColor tag, or regular Create package from getPartialFromTagColor() method
                 PartialModel model = ColoredPackagePartialUtil.getPartialFromTagColor(blockEntity.animatedPackage); // Get colored partial model from ItemStack if possible
 
-                BlockState dummyState;
-//                switch () {
-//                    case "red" ->
-//                }
-
-                ColoredPackages.LOGGER.info("model render: {}", model);
-
                 if (key != null) {
                     SuperByteBuffer rigBuffer = CachedBuffers.partial((PartialModel) AllPartialModels.PACKAGE_RIGGING.get(key), blockEntity.getBlockState());
-                    SuperByteBuffer boxBuffer = CachedBuffers.partial(model, AllDyedBlocks.DYED_PACKAGER.getDefaultState());
-                    ColoredPackages.LOGGER.info("box buffer: {}", boxBuffer);
+                    SuperByteBuffer boxBuffer = CachedBuffers.partial(model, blockEntity.getBlockState());
                     boolean animating = blockEntity.isAnimationInProgress();
                     boolean depositing = blockEntity.currentlyDepositing;
 
                     for(SuperByteBuffer buf : new SuperByteBuffer[]{boxBuffer, rigBuffer}) {
-                        ((SuperByteBuffer)((SuperByteBuffer)((SuperByteBuffer)((SuperByteBuffer)((SuperByteBuffer)buf.translate(0.0F, 0.1875F, 0.0F)).translate(diff.normalize().scale((double)itemDistance).subtract((double)0.0F, animating && depositing ? (double)0.75F : (double)0.0F, (double)0.0F))).center()).scale(scale)).uncenter()).light(light).overlay(overlay).renderInto(ms, buffer.getBuffer(RenderType.cutout()));
+                        buf.translate(0.0F, 0.1875F, 0.0F).translate(diff.normalize().scale((double)itemDistance).subtract((double)0.0F, animating && depositing ? (double)0.75F : (double)0.0F, (double)0.0F)).center().scale(scale).uncenter().light(light).overlay(overlay).renderInto(ms, buffer.getBuffer(RenderType.cutout()));
                         if (!blockEntity.currentlyDepositing) {
                             break;
                         }
                     }
-
+                    ci.cancel();
                 }
             }
         }
