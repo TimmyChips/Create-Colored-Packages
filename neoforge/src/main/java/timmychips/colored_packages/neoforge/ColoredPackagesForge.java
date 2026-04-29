82d624cf2c68931ed4900ec2c21bc226cf66e285
TimmyChips <jhinners@outlook.com>
2026-04-29 02:32:36 -0500

    Fixed Mechanical Arm unable to set Dyed Packager and Dyed Re-Packagers as interaction points

--- neoforge/src/main/java/timmychips/colored_packages/neoforge/ColoredPackagesForge.java
+++ neoforge/src/main/java/timmychips/colored_packages/neoforge/ColoredPackagesForge.java
@@ -2,27 +2,29 @@ package timmychips.colored_packages.neoforge;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.RegisterEvent;
+import timmychips.colored_packages.AllDyedArmInteractionPointTypes;
 import timmychips.colored_packages.ColoredPackages;
 
 @Mod(ColoredPackages.MOD_ID)
 public final class ColoredPackagesForge {
     public ColoredPackagesForge(IEventBus bus) {
-        // Submit our event bus to let Architectury API register our content on the right time.
-//        EventBuses.registerModEventBus(ColoredPackages.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-
-        //noinspection removal;
-//        EventBuses.registerModEventBus(ColoredPackages.MOD_ID, bus); // Register Event Bus for Forge and NeoForge
-
         // Run our common setup.
         ColoredPackages.REGISTRATE.registerEventListeners(bus);
         ColoredPackages.init();
         AllDyedBlockEntityTypesForge.register();
         AllPackageEntityTypesForge.register();
         bus.addListener(AllPackageEntityTypesForge::registerEntityAttributes);
-//        ModifyCreativeMenuForge.modify(bus);
         bus.addListener(ModifyCreativeMenuForge::addTaggedPackagesForge);
+        // Reload listener
+        bus.addListener(AllCapabilitiesEvent::register);
+        bus.addListener(ColoredPackagesForge::onRegister);
         // Client bus listener
         bus.addListener(ColoredPackagesClientForge::init);
-        bus.addListener(AllCapabilitiesEvent::register);
+    }
+
+    // After registering vanilla registries, initialize modded objects (so it doesn't freeze/crash)
+    public static void onRegister(RegisterEvent event) {
+        AllDyedArmInteractionPointTypes.init();
     }
 }
