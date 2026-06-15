5d07b2c113c6f2004d8d93cfcaa689c0e24c2ddc
TimmyChips <jhinners@outlook.com>
2026-06-15 00:44:35 -0500

    Removed debug mixin export

--- neoforge/src/main/java/timmychips/colored_packages/neoforge/mixin/compat/vibrantvaults/PackagerBEMixin.java
+++ neoforge/src/main/java/timmychips/colored_packages/neoforge/mixin/compat/vibrantvaults/PackagerBEMixin.java
@@ -7,7 +7,6 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.neoforge.items.ItemStackHandler;
-import org.spongepowered.asm.mixin.Debug;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -17,7 +16,6 @@ import java.util.Optional;
 
 import static timmychips.colored_packages.compat.VibrantVaultsCompat.VIBRANT_PACKAGERS_MAP;
 
-@Debug(export = true)
 @Mixin(PackagerBlockEntity.class)
 public class PackagerBEMixin {
 
